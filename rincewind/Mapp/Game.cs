d039800aa262a86dc716c5debfb6b4418c0b0dae
Aaron Bolyard <adb@aaronbolyard.com>
2018-03-09 10:24:34 -0500

    Initial work on managed Mapp implementation.

---
+++ rincewind/Mapp/Game.cs
@@ -0,0 +1,132 @@
+﻿// RINCEWIND
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright [bk]door.maus
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dormouse.Rincewind.Twoflower;
+
+namespace Dormouse.Rincewind.Mapp
+{
+	public class Game
+	{
+		Brochure mBrochure;
+
+		public Brochure Brochure
+		{
+			get { return mBrochure; }
+		}
+
+		Dictionary<string, int> mResourceTypeIDs = new Dictionary<string, int>();
+		public void SetResourceType(Resource resource, string name)
+		{
+			name = name?.ToLowerInvariant();
+
+			int id;
+			if (!mResourceTypeIDs.TryGetValue(name, out id))
+			{
+				throw new InvalidOperationException(String.Format("Resource type {0} not found; cannot assign to resource #{1} ('{2}').", name, resource.ID, resource.Name));
+			}
+
+			resource.TypeID = id;
+			resource.TypeName = name;
+		}
+
+		public bool IsResourceType(Resource resource, string name)
+		{
+			name = name?.ToLowerInvariant();
+
+			int id;
+			if (!mResourceTypeIDs.TryGetValue(name, out id))
+			{
+				throw new InvalidOperationException(String.Format("Resource type {0} not found; cannot compare to resource #{1} ('{2}').", name, resource.ID, resource.Name));
+			}
+
+			return resource.ID == id;
+		}
+
+		Dictionary<string, int> mActionDefinitionIDs = new Dictionary<string, int>();
+		public void SetActionDefinition(ResourceAction action, string name)
+		{
+			name = name?.ToLowerInvariant();
+
+			int id;
+			if (!mActionDefinitionIDs.TryGetValue(name, out id))
+			{
+				throw new InvalidOperationException(String.Format("Resource action definition {0} not found; cannot assign to action.", name));
+			}
+
+			action.TypeID = id;
+			action.TypeName = name;
+		}
+
+		public Game(Brochure brochure)
+		{
+			if (brochure == null)
+			{
+				throw new ArgumentNullException("brochure");
+			}
+
+			mBrochure = brochure;
+			ValidateBrochure();
+		}
+
+		public const string ITEM_RESOURCE = "item";
+		public const string SKILL_RESOURCE = "skill";
+		public const string QUEST_RESOURCE = "quest";
+		public const string QUEST_STEP_RESOURCE = "quest_step";
+
+		public const string QUEST_COMPLETE_ACTION = "quest.complete";
+
+		void ValidateBrochure()
+		{
+			EnsureResourceType("item");
+			EnsureResourceType("skill");
+			EnsureResourceType("quest");
+			EnsureResourceType("quest_step");
+
+			EnsureActionType("quest.complete");
+		}
+
+		void EnsureResourceType(string name)
+		{
+			var types = mBrochure.GetResourceTypes(name);
+			if (types.Length == 0)
+			{
+				int id = mBrochure.AddResourceType(name);
+				mResourceTypeIDs.Add(name, types[0]);
+			}
+			else if (types.Length == 1)
+			{
+				mResourceTypeIDs.Add(name, types[0]);
+			}
+			else
+			{
+				throw new InvalidOperationException(String.Format("Resource type {0} has multiple {1} definitions, expected 1.", name, types.Length));
+			}
+		}
+
+		void EnsureActionType(string name)
+		{
+			var actions = new ResourceActionCollection(mBrochure);
+			var types = actions.Definitions().Where(r => r.TypeName == name).ToArray();
+			if (types.Length == 0)
+			{
+				int id = mBrochure.AddActionDefinition(new ResourceAction() { TypeName = name });
+				mActionDefinitionIDs.Add(name, id);
+			}
+			else if (types.Length == 1)
+			{
+				mActionDefinitionIDs.Add(name, types[0].TypeID);
+			}
+			else
+			{
+				throw new InvalidOperationException(String.Format("Action definition {0} has multiple {1} instances, expected 1.", name, types.Length));
+			}
+		}
+	}
+}
---
+++ rincewind/Mapp/MappResource.cs
@@ -0,0 +1,77 @@
+﻿// RINCEWIND
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright [bk]door.maus
+using System;
+using Dormouse.Rincewind.Twoflower;
+
+namespace Dormouse.Rincewind.Mapp
+{
+	public abstract class MappResource : IDisposable
+	{
+		Resource mResource;
+		public Resource Resource
+		{
+			get { return mResource; }
+		}
+
+		public bool IsDisposed
+		{
+			get { return mResource == null || mResource.IsDisposed; }
+		}
+
+		Game mGame;
+		public Game Game
+		{
+			get { return mGame; }
+		}
+
+		public MappResource(Game game)
+		{
+			if (game == null)
+			{
+				throw new ArgumentNullException("game");
+			}
+
+			mGame = game;
+
+			mResource = new Resource();
+		}
+
+		protected abstract bool TryFromResource(Resource resource);
+
+		public void FromResource(Resource resource)
+		{
+			if (resource == null)
+			{
+				throw new ArgumentNullException("resource");
+			}
+
+			if (!TryFromResource(resource))
+			{
+				throw new InvalidOperationException(String.Format("Could not create {0} from resource '{1}' (#{2}).", GetType().Name, resource.Name, resource.ID));
+			}
+
+			mResource = resource;
+		}
+
+		~MappResource()
+		{
+			Dispose();
+		}
+
+		public virtual void Dispose()
+		{
+			if (!IsDisposed)
+			{
+				mResource.Dispose();
+				mResource = null;
+
+				GC.SuppressFinalize(this);
+			}
+		}
+	}
+}
