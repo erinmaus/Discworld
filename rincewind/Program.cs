261f6a5f7dcf8e8a0bce3f97bd49a8fd855bf23b
Aaron Bolyard <adb@aaronbolyard.com>
2018-05-17 08:01:21 -0400

    Example, in C# and C++.

--- rincewind/Program.cs
+++ rincewind/Program.cs
@@ -6,7 +6,9 @@
 //
 // Copyright [bk]door.maus
 using System;
+using System.Collections.Generic;
 using Dormouse.Rincewind.Twoflower;
+using Dormouse.Rincewind.Mapp;
 
 namespace Dormouse.Rincewind
 {
@@ -14,7 +16,104 @@ namespace Dormouse.Rincewind
 	{
 		public static void Main()
 		{
-			// Nothing.
+			Brochure brochure = new Brochure(null);
+			Game game = new Game(brochure);
+
+			ItemResource coinsItem = new ItemResource(game);
+			coinsItem.Create();
+			coinsItem.Name = "Coins";
+			coinsItem.Value = 1;
+
+			ItemResource sardinesItem = new ItemResource(game);
+			sardinesItem.Create();
+			sardinesItem.Name = "Sardines";
+			sardinesItem.Value = 10;
+
+			SkillResource cookingSkill = new SkillResource(game);
+			cookingSkill.Create();
+			cookingSkill.Name = "Cooking";
+
+			QuestStepResource startQuest = new QuestStepResource(game);
+			startQuest.Create();
+			startQuest.Name = "(Cook's Assistant) Start Quest";
+
+			QuestStepResource getEggStep = new QuestStepResource(game);
+			getEggStep.Create();
+			getEggStep.Name = "(Cook's Assistant) Get large egg";
+			getEggStep.AddSolution(startQuest);
+
+			QuestStepResource getFlourStep = new QuestStepResource(game);
+			getFlourStep.Create();
+			getFlourStep.Name = "(Cook's Assistant) Get finely milled flour";
+			getFlourStep.AddSolution(startQuest);
+
+			QuestStepResource getMilkStep = new QuestStepResource(game);
+			getMilkStep.Create();
+			getMilkStep.Name = "(Cook's Assistant) Get prized milk";
+			getMilkStep.AddSolution(startQuest);
+
+			QuestStepResource finalStep = new QuestStepResource(game);
+			finalStep.Create();
+			finalStep.Name = "(Cook's Assistant) Make cake";
+			finalStep.AddSolution(getEggStep, getFlourStep, getMilkStep);
+
+			QuestResource cooksAssistantQuest = new QuestResource(game);
+			cooksAssistantQuest.Create();
+			cooksAssistantQuest.Name = "Cook's Assistant";
+			cooksAssistantQuest.SetFinalStep(finalStep);
+
+			Dictionary<SkillResource, float> xpReward = new Dictionary<SkillResource, float>();
+			xpReward.Add(cookingSkill, 300.0f);
+			cooksAssistantQuest.RewardXP = xpReward;
+
+			brochure.Connect(new Requirement() { Count = 500, IsOutput = true }, cooksAssistantQuest.CompleteAction, coinsItem.Resource);
+			brochure.Connect(new Requirement() { Count = 20, IsOutput = true }, cooksAssistantQuest.CompleteAction, sardinesItem.Resource);
+
+			var deserializedCooksAssistantQuest = new QuestResource(game);
+			deserializedCooksAssistantQuest.FromResource(cooksAssistantQuest.Resource);
+
+			var resources = new ResourceCollection(brochure);
+			foreach (var resource in resources.All())
+			{
+				Console.WriteLine("'{0}' (#{1}, {2})", resource.Name, resource.ID, resource.TypeName);
+
+				var actions = new ResourceActionCollection(brochure, resource);
+				foreach (var action in actions.All())
+				{
+					if (action.IsGetter)
+					{
+						Console.WriteLine("- get {0}", action.TypeName);
+					}
+					else
+					{
+						Console.WriteLine("- {0}", action.TypeName);
+					}
+
+					var requirements = new RequirementCollection(brochure, action);
+					foreach (var requirement in requirements.All())
+					{
+						string type;
+						if (requirement.IsInput && requirement.IsOutput)
+						{
+							type = "in/out [bad!]";
+						}
+						else if (requirement.IsInput)
+						{
+							type = "input";
+						}
+						else if (requirement.IsOutput)
+						{
+							type = "output";
+						}
+						else
+						{
+							type = "require";
+						}
+
+						Console.WriteLine("  * {0} '{1}' (#{2}, {3}) x{4}", type, requirement.Resource.Name, requirement.Resource.ID, requirement.Resource.TypeName, (int)Math.Ceiling(requirement.Count));
+					}
+				}
+			}
 		}
 	}
 }
--- test/basic_item/source/main.cpp
+++ test/basic_item/source/main.cpp
@@ -2,20 +2,58 @@
 #include "twoflower/brochure.hpp"
 #include "mapp/game/game.hpp"
 #include "mapp/game/item.hpp"
+#include "mapp/game/shop.hpp"
 
 int main()
 {
-	twoflower::Brochure brochure("sample.gamedb");
+	twoflower::Brochure brochure;
 	mapp::Game game(brochure);
 
-	mapp::ItemDefinition item;
-	item.name = "Amulet of Yendor";
-	game.add(item);
+	mapp::ItemDefinition item_definition;
+	item_definition.name = "Amulet of Yendor";
+	auto item = game.add(item_definition);
+
+	mapp::ShopDefinition shop_definition;
+	shop_definition.name = "Amulet Shoppe";
+	auto shop = game.add(shop_definition);
+	game.add(shop, item, 10);
 
 	auto resources = brochure.resources();
 	for (auto i = resources.begin(); i != resources.end(); ++i)
 	{
 		std::cout << "name: " << i->get_name() << ", id: " << i->get_id() << std::endl;
+
+		auto actions = brochure.actions(*i);
+		for (auto j = actions.begin(); j != actions.end(); ++j)
+		{
+			std::cout << " - " << j->get_type().name << std::endl;
+			auto requirements = brochure.requirements(*j);
+			for (auto k = requirements.begin(); k != requirements.end(); ++k)
+			{
+				std::string result;
+				if (k->is_input())
+				{
+					result = "input";
+				}
+				else if (k->is_output())
+				{
+					result = "output";
+				}
+				else
+				{
+					result = "req";
+				}
+
+				std::cout << "  - (" << result << ") " << (int)k->get_count() << ", " << k->get_resource().get_name() << std::endl;
+			}
+		}
+	}
+
+	std::cout << "inventory:" << std::endl;
+	auto inventory = game.inventory(shop);
+	for (auto item: inventory)
+	{
+		std::cout << " - " << item.resource.get_name() << std::endl;
 	}
 
 	return 0;
