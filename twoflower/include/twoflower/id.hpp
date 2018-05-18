521bc9b24154a9e7308b923c3e14fac8fe18ed5c
Aaron Bolyard <adb@aaronbolyard.com>
2018-05-18 05:07:25 -0400

    Added ID type.

---
+++ twoflower/include/twoflower/id.hpp
@@ -0,0 +1,43 @@
+// TWOFLOWER
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright [bk]door.maus
+
+#ifndef TWOFLOWER_ID_HPP
+#define TWOFLOWER_ID_HPP
+
+namespace twoflower
+{
+	struct ID
+	{
+	public:
+		ID();
+		ID(const ID& other) = default;
+		ID(ID&& other);
+		ID(int value);
+		~ID() = default;
+
+		operator int();
+
+		bool operator <(const ID& other);
+		bool operator >(const ID& other);
+		bool operator <=(const ID& other);
+		bool operator >=(const ID& other);
+		bool operator ==(const ID& other);
+		bool operator !=(const ID& other);
+
+		ID& operator =(int id);
+		ID& operator =(const ID& other) = default;
+		ID& operator =(ID&& other);
+
+		static const ID NIL;
+
+	private:
+		int id = 0;
+	};
+}
+
+#endif
---
+++ twoflower/source/id.cpp
@@ -0,0 +1,88 @@
+// TWOFLOWER
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright [bk]door.maus
+
+#include <cassert>
+#include <stdexcept>
+#include "twoflower/id.hpp"
+
+const twoflower::ID twoflower::ID::NIL = twoflower::ID(0);
+
+twoflower::ID::ID()
+{
+	// Nothing.
+}
+
+twoflower::ID::ID(int value)
+{
+	if (value < 0)
+	{
+		throw std::runtime_error("id must be non-negative");
+	}
+}
+
+twoflower::ID::ID(ID&& other)
+{
+	*this = other;
+}
+
+twoflower::ID::operator int()
+{
+	return id;
+}
+
+bool twoflower::ID::operator <(const ID& other)
+{
+	return id < other.id;
+}
+
+bool twoflower::ID::operator >(const ID& other)
+{
+	return id > other.id;
+}
+
+bool twoflower::ID::operator <=(const ID& other)
+{
+	return id <= other.id;
+}
+
+bool twoflower::ID::operator >=(const ID& other)
+{
+	return id >= other.id;
+}
+
+bool twoflower::ID::operator ==(const ID& other)
+{
+	return id == other.id;
+}
+
+bool twoflower::ID::operator !=(const ID& other)
+{
+	return !(*this == other);
+}
+
+twoflower::ID& twoflower::ID::operator =(int value)
+{
+	if (value < 0)
+	{
+		throw std::runtime_error("id must be non-negative");
+	}
+
+	id = value;
+
+	return *this;
+}
+
+twoflower::ID& twoflower::ID::operator =(ID&& other)
+{
+	assert(!(other.id < 0) && "other ID is negative");
+
+	id = other.id;
+	other.id = 0;
+
+	return *this;
+}
